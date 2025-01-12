3a8ac6930512fad951677f939cee95c04127ba6b
ElizabethAnnDavis <immakitty88@gmail.com>
2025-01-12 12:15:41 -0500

    displayResults working

--- app.js
+++ app.js
@@ -69,6 +69,13 @@ function runQuiz(){
     console.log(quizQuestions.length);
 
     displayQuestion();
+
+    //console.log("currentQuestionIndex: " + currentQuestionIndex);
+    //console.log("quizQuestions.length: " + quizQuestions.length);
+
+    //if (currentQuestionIndex >= quizQuestions.length) {
+      //  displayResults();
+    //};
     //for(let i = 0; i < quizQuestions.length; i++){
         //if(i != 0){
           //  clearForm();
@@ -109,18 +116,16 @@ function runQuiz(){
         };*/
         
     //};
-    if (currentQuestionIndex >= quizQuestions.length) {
-        displayResults();
-    }
 }
 
 // Function to display the question and answers
 function displayQuestion() {
     const formContainer = document.querySelector('.center');
     formContainer.innerHTML = '';  // Clear any previous content
-    
+
     if (currentQuestionIndex >= quizQuestions.length) {
         pageTitle.innerHTML = "Thank you for completing the quiz!";
+        displayResults();
         return;
     }else{
         pageTitle.innerHTML = `QUESTION ${currentQuestionIndex+1}`;
@@ -184,19 +189,23 @@ function displayQuestion() {
 
 function displayResults(){
     if(score === quizQuestions.length){
-        alert(`PERFECT SCORE!\nCongrats you got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
-        console.log(`PERFECT SCORE!\nCongrats you got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+        alert(`Follow Your Passion and Life Will Reward You.\nPERFECT SCORE!\nCongrats you got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+        console.log(`Follow Your Passion and Life Will Reward You.\nPERFECT SCORE!\nCongrats you got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
     }else if(score <= quizQuestions.length/2){
         if(score === 0){
             alert(`You must redeem your honour!?\n${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
             console.log(`You must redeem your honour!\n${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
         }else{
-            alert(`You need to focus.\nHave some calming tea.\n${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
-            console.log(`You need to focus.\nHave some calming tea.\n${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+            alert(`Failure is only the opportunity to begin again. Only this time, more wisely.\n${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+            console.log(`Failure is only the opportunity to begin again. Only this time, more wisely.\n${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+            //You need to focus.\nHave some calming tea.
         };
+    }else if(score <= (quizQuestions.length/4)*3){
+        alert(`Good times become good memories, but bad times make good lessons.\nYou got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+        console.log(`Good times become good memories, but bad times make good lessons.\nYou got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
     }else{
-        alert(`You got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
-        console.log(`You got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+        alert(`It is usually best to admit mistakes when they occur, and to seek to restore honor.\nYou got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
+        console.log(`It is usually best to admit mistakes when they occur, and to seek to restore honor.\nYou got ${score}/${quizQuestions.length}, ${(score/quizQuestions.length)*100}%`);
     };
     startBtn.style.display = 'block';
 }
